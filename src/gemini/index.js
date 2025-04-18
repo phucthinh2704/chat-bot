bab4a359d30e21b8e8e5e57915c05bbea19170aa
Tran Nguyen Phuc Thinh <thinhphuc2704@gmail.com>
2025-04-18 22:58:31 +0700

    update model

--- src/gemini/index.js
+++ src/gemini/index.js
@@ -17,7 +17,7 @@ const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-	model: "gemini-2.0-flash-exp-image-generation",
+	model: "gemini-2.0-flash-thinking-exp-01-21",
 });
 
 const generationConfig = {
